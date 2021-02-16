da7c1c6aeca935f3c43ac901b2d1a2a51da13255
lfelipearaujo <63312737+lfelipearaujo@users.noreply.github.com>
2021-02-16 16:44:42 -0500

    c2

--- app.js
+++ app.js
@@ -19,12 +19,15 @@ pageUpButton.addEventListener('click', goToTop);
 //! nav toggle
 
 const burger = document.querySelector('.burger');
+const liClose = document.querySelectorAll('.li_mob');
+
+
 
 function navToggle(e) {
   if (!e.target.classList.contains('active')) {
     e.target.classList.add('active');
-    gsap.to('.line1', .5, { rotate: '45', y: 7, background:'black' })
-    gsap.to('.line2', .5, { rotate:'-45', y: -7, background:'black' })
+    gsap.to('.line1', .5, { rotate: '45', y: 7 })
+    gsap.to('.line2', .5, { rotate:'-45', y: -7 })
     gsap.to(".nav-bar", 1, { clipPath: "circle(2500px at 100% -10%" });
     document.body.classList.add('hide')
   } else {
@@ -38,4 +41,22 @@ function navToggle(e) {
   }
 }
 
+
+function closeNavBar(e) {
+
+  if (!burger.classList.contains('active')) {
+    console.log('it work');
+    e.target.classList.remove("active");
+    gsap.to(".nav-bar", 1, { clipPath: "circle(50px at 100% -10%" });
+  gsap.to(".line1", 0.5, { rotate: "0", y: 0, background: "white" });
+    gsap.to(".line2", 0.5, { rotate: "0", y: 0, background: "white" });
+
+  }
+
+  }
+
 burger.addEventListener('click', navToggle);
+
+liClose.forEach(li  => {;
+  li.addEventListener('click', closeNavBar)
+});
